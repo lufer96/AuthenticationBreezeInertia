8c3953f9de36975a239cd64b5f094bba14dda919
lufer96 <lufer96>
2022-04-01 21:49:40 -0500

    👌 IMPROVE: Add trait to task model , and relationship user model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuid;
 
     /**
      * The attributes that are mass assignable.
