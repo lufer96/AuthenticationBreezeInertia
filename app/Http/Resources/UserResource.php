9aa8e273fec301c9bff141c8effd69110bda9a13
lufer96 <lufer96>
2022-04-12 21:42:21 -0500

    👌 IMPROVE: Relationship to user and resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Task\Resources;
+namespace App\Http\Resources;
 
-use App\Http\Resources\UserResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TaskResource extends JsonResource
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,11 +15,8 @@ class TaskResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'uuid_key' => $this->uuid_key,
             'name' => $this->name,
-            'description' => $this->description,
-            'status' => (bool) $this->status,
-            'user' => new UserResource($this->user),
+            'email' => $this->email,
         ];
     }
 }
