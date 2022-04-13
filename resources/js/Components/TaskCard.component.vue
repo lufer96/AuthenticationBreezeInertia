454e5c893a3faad349320b6ff755477a36457e75
lufer96 <lufer96>
2022-04-12 21:44:47 -0500

    👌 IMPROVE: Task listing page and task card component

---
+++ resources/js/Components/TaskCard.component.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+    task: {
+        type: Object,
+        default: () => ({}),
+    },
+});
+const taskTextStatus = computed(() => (props.task.status ? "Completed" : "Pending"))
+;
+</script>
+<template>
+    <div
+        class="max-w-sm rounded shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+    >
+        <div class="px-6 py-6">
+            <span class="font-bold text-xl mb-2">
+                {{ task.name }}
+            </span>
+            <p class="text-gray-700 text-base pt-4">
+                {{ task.description }}
+            </p>
+        </div>
+        <div class="px-6 pt-4 pb-4 flex justify-between">
+            <span>{{ task.user.name }}</span>
+            <div class="form-check">
+                <input
+                    class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                    type="checkbox"
+                    v-model="task.status"
+                    id="flexCheckChecked"
+                />
+                <label
+                    class="form-check-label inline-block text-gray-800 w-20"
+                    for="flexCheckChecked"
+                >
+                    {{ taskTextStatus }}
+                </label>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -1,15 +1,27 @@
 <script setup>
 import BreezeGuestLayout from "@/Layouts/Guest.vue";
 import { Head, usePage } from "@inertiajs/inertia-vue3";
+import TasKCardComponent from "@/Components/TaskCard.component.vue";
 
 defineProps({
-    tasks: Object
-})
+    tasks: Object,
+});
+
 </script>
 
 <template>
-        <Head title="List Tasks" />
-        <div class="text-lg">Tasks</div>
-        {{ tasks}}
-        {{ $page.props.tasks}}
+    <Head title="List Tasks" />
+    <div class="container pt-10 mx-auto w-5/6">
+        <div class="flex justify-between">
+            <span class="text-lg font-bold"> List task </span>
+            <button
+                class="px-4 py-2 bg-blue-400 hover:bg-blue-300 rounded-lg font-semibold tex-sm text-white"
+            >
+                Create new task
+            </button>
+        </div>
+        <div  class="flex flex-wrap gap-8 mt-10 justify-between">
+            <TasKCardComponent v-for="task in tasks.data" :key="task.uuid_key" :task="task" />
+        </div>
+    </div>
 </template>
